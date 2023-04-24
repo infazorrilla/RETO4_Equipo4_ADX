6b9d81f7c2fd7686d85d9dc7277b9f7b6ea8f85b
in1dw3 <in1dw3@HZ352016.elorrieta.int>
2023-04-24 20:29:37 +0200

    pojo pacient

--- RETO4_Equipo4_ADX/src/model/pojos/Sanitarian.java
+++ RETO4_Equipo4_ADX/src/model/pojos/Sanitarian.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Objects;
 
-public class Sanitarian implements Serializable {
+public class Sanitarian extends User implements Serializable  {
 
 	private static final long serialVersionUID = -6651744658859466141L;
 
@@ -12,52 +12,50 @@ public class Sanitarian implements Serializable {
 	private float salary;
 	private String type;
 	private ArrayList<Appointment> appointments;
-
+	
 	// GETTERS AND SETTERS //
 	public int getStaffNum() {
 		return staffNum;
 	}
-
 	public void setStaffNum(int staffNum) {
 		this.staffNum = staffNum;
 	}
-
 	public float getSalary() {
 		return salary;
 	}
-
 	public void setSalary(float salary) {
 		this.salary = salary;
 	}
-
 	public String getType() {
 		return type;
 	}
-
 	public void setType(String type) {
 		this.type = type;
 	}
-
 	public ArrayList<Appointment> getAppointments() {
 		return appointments;
 	}
-
 	public void setAppointments(ArrayList<Appointment> appointments) {
 		this.appointments = appointments;
 	}
-
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
 	// HASHCODE //
+	
 	@Override
 	public int hashCode() {
-		return Objects.hash(appointments, salary, staffNum, type);
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + Objects.hash(appointments, salary, staffNum, type);
+		return result;
 	}
-
-	// EQUALS //
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
-		if (obj == null)
+		if (!super.equals(obj))
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
@@ -66,12 +64,20 @@ public class Sanitarian implements Serializable {
 				&& Float.floatToIntBits(salary) == Float.floatToIntBits(other.salary) && staffNum == other.staffNum
 				&& Objects.equals(type, other.type);
 	}
-
-	// TO STRING //
 	@Override
 	public String toString() {
 		return "Sanitarian [staffNum=" + staffNum + ", salary=" + salary + ", type=" + type + ", appointments="
 				+ appointments + "]";
 	}
 
+	
+	
+
+	
+	
+	
+	
+	// TO STRING //
+	
+	
 }
