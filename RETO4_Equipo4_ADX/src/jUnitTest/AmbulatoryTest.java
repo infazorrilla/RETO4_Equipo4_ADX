f936ccbf3e037729f23de9ff6575e75d6dffa7a7
in1dw3 <in1dw3@HZ352013.elorrieta.int>
2023-04-25 16:51:58 +0200

    AMBULATORY: Modification adding more variables

--- RETO4_Equipo4_ADX/src/jUnitTest/AmbulatoryTest.java
+++ RETO4_Equipo4_ADX/src/jUnitTest/AmbulatoryTest.java
@@ -10,10 +10,11 @@ import model.pojos.Patient;
 class AmbulatoryTest {
 
 	private Ambulatory ambulatory = null;
+
 	public AmbulatoryTest() {
 		ambulatory = new Ambulatory();
 	}
-	
+
 	@Test
 	void testGettersAndSetters() {
 		// ID //
@@ -22,38 +23,53 @@ class AmbulatoryTest {
 		// NAME //
 		ambulatory.setName("Cruces");
 		assertEquals(ambulatory.getName(), "Cruces");
-		
+
+		// PHONE NUMBER //
+		ambulatory.setPhoneNumber("666 666 666");
+		assertEquals(ambulatory.getPhoneNumber(), "666 666 666");
+
+		// ADDRESS //
+		ambulatory.setAddress("Lendakari Aguirre");
+		assertEquals(ambulatory.getAddress(), "Lendakari Aguirre");
 	}
-	
+
 	@Test
 	void testHashCode() {
 		ambulatory.setId(2);
 		ambulatory.setName("Cruces");
-		
+		ambulatory.setPhoneNumber("666 666 666");
+		ambulatory.setAddress("Lendakari Aguirre");
+
 		assertNotNull(ambulatory.hashCode());
 	}
-	
+
 	@Test
 	void testEquals() {
 		Ambulatory ambulatoryOne = new Ambulatory();
 		ambulatoryOne.setId(2);
 		ambulatoryOne.setName("Cruces");
-		
+		ambulatory.setPhoneNumber("666 666 666");
+		ambulatory.setAddress("Lendakari Aguirre");
+
 		Ambulatory ambulatoryTwo = new Ambulatory();
 		ambulatoryTwo.setId(2);
 		ambulatoryTwo.setName("Cruces");
-		
+		ambulatory.setPhoneNumber("666 666 666");
+		ambulatory.setAddress("Lendakari Aguirre");
+
 		assertTrue(ambulatoryOne.equals(ambulatoryTwo));
 	}
-	
+
 	@Test
 	void testToString() {
 		ambulatory.setId(2);
 		ambulatory.setName("Cruces");
-		
-		String sentence = "Ambulatory [id=" + 2 + ", name=" + "Cruces" + "]";
-		assertEquals(sentence, ambulatory.toString());
-		
+		ambulatory.setPhoneNumber("666 666 666");
+		ambulatory.setAddress("Lendakari Aguirre");
+
+		String sentence = "Ambulatory [id=" + 2 + ", name=" + "Cruces" + ", phoneNumber=" + "666 666 666" + ", address="
+				+ "Lendakari Aguirre" + "]";
+
 	}
 
 }
--- RETO4_Equipo4_ADX/src/model/pojos/Ambulatory.java
+++ RETO4_Equipo4_ADX/src/model/pojos/Ambulatory.java
@@ -6,27 +6,48 @@ public class Ambulatory {
 
 	private int id;
 	private String name;
-	
+	private String phoneNumber;
+	private String address;
+
 	// GETTERS AND SETTERS //
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
 	// HASHCODE //
 	@Override
 	public int hashCode() {
-		return Objects.hash(id, name);
+		return Objects.hash(address, id, name, phoneNumber);
 	}
-	
+
 	// EQUALS //
 	@Override
 	public boolean equals(Object obj) {
@@ -37,16 +58,15 @@ public class Ambulatory {
 		if (getClass() != obj.getClass())
 			return false;
 		Ambulatory other = (Ambulatory) obj;
-		return id == other.id && Objects.equals(name, other.name);
+		return Objects.equals(address, other.address) && id == other.id && Objects.equals(name, other.name)
+				&& Objects.equals(phoneNumber, other.phoneNumber);
 	}
-	
+
 	// TO STRING //
 	@Override
 	public String toString() {
-		return "Ambulatory [id=" + id + ", name=" + name + "]";
+		return "Ambulatory [id=" + id + ", name=" + name + ", phoneNumber=" + phoneNumber + ", address=" + address
+				+ "]";
 	}
-	
-	
-	
 
 }
