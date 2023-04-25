f6ad2fb09ec191dc5d68088610a6f5b6d25314c0
in1dw3 <in1dw3@HZ352016.elorrieta.int>
2023-04-25 18:07:04 +0200

    appointment manager cositis

--- RETO4_Equipo4_ADX/src/manager/AppointmentManager.java
+++ RETO4_Equipo4_ADX/src/manager/AppointmentManager.java
@@ -2,27 +2,26 @@ package manager;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import model.pojos.Appointment;
-import model.pojos.Patient;
-import model.pojos.Sanitarian;
-import model.pojos.TimeSlot;
+
 import model.utils.BBDDUtils;
 
 public class AppointmentManager extends AbstractManager<Appointment>{
 	
-	public static final String APPOINTMENT_TABLE = "cita";
+	public static final String APPOINTMENT_TABLE = "cita"; 
 	
 	@Override
-	public List<Appointment> select() throws SQLException, Exception{
-		ArrayList<Appointment> ret = null;
-		String sql = "select * from " + APPOINTMENT_TABLE;
+	public Appointment select(int id) throws SQLException, Exception{
+		Appointment ret = null;
+		
+		String sql = "select * from " + APPOINTMENT_TABLE  + " where id=" +id;
 
 		Connection connection = null;
 		Statement statement = null;
@@ -32,30 +31,36 @@ public class AppointmentManager extends AbstractManager<Appointment>{
 		connection = DriverManager.getConnection(BBDDUtils.URL_LOCAL, BBDDUtils.USER_LOCAL, BBDDUtils.PASS_LOCAL);
 		statement = connection.createStatement();
 		resultSet = statement.executeQuery(sql);
+		return ret;
+
+	}
+
+	@Override
+	public List<Appointment> select() throws SQLException, Exception {
+		ArrayList<Appointment> ret = null;
+
+		String sql = "select * from " + APPOINTMENT_TABLE;
+
+		Connection connection = null;
+		Statement statement = null;
+		ResultSet resultSet = null;
+		
+		Class.forName(BBDDUtils.DRIVER_LOCAL);
+		connection = DriverManager.getConnection(BBDDUtils.URL_LOCAL, BBDDUtils.USER_LOCAL, BBDDUtils.PASS_LOCAL);
+		statement = connection.createStatement();
+		resultSet = statement.executeQuery(sql);   
 
 		while (resultSet.next()) {
 			if (null == ret)
 				ret = new ArrayList<Appointment>();
-
+			
 			int id = resultSet.getInt("id");
-			String type = resultSet.getString("tipo");
-			
-			//No sé si va a ser médico o enfermero
-//			Sanitarian sanitarian = new Sanitarian();
-			
-			Patient patient = new Patient();
-			int patientId = resultSet.getInt("dniPaciente");
-//			patient.set
-			//No puedo hacerlo porque está mal el POJO
-			
-//			TimeSlot = timeSlot.getInt("idFranja");
-//			patient.set
-			//No puedo hacerlo porque no está corregida la BBDD
 
-//			Appointment appointment = new Appointment(id, type, sanitarians, patient, timeSlot);
+			Appointment appointment = new Appointment();
+			appointment.setId(id);
+			
 
-
-//			ret.add(appointment);
+			ret.add(appointment);
 		}
 
 		resultSet.close();
@@ -67,71 +72,24 @@ public class AppointmentManager extends AbstractManager<Appointment>{
 			connection.close();
 
 		return ret;
-	}
-
-	@Override
-	public void insert(Appointment appointment) throws SQLException, Exception {
-		Connection connection = DriverManager.getConnection(BBDDUtils.URL_LOCAL, BBDDUtils.USER_LOCAL,
-				BBDDUtils.PASS_LOCAL);
-		Statement statement = connection.createStatement();
-		Class.forName(BBDDUtils.DRIVER_LOCAL);
-
-//		Cómo meto el id del Sanitario (puede ser Médico o Enfermero)
-		String sql = "insert into " + APPOINTMENT_TABLE + " (tipo, dniPaciente, dniSanitario, idFranja) values ('" + appointment.getType()
-				+ "', '" + /*appointment.getPatient().getId() +*/ "', '" + /*appointment.getSanitarian() +*/ "')";
-//		No puedo meter el paciente porque el POJO está mal
-
-		statement.executeUpdate(sql);
-
-		if (statement != null)
-			statement.close();
-
-		if (connection != null)
-			connection.close();
-
-	}
-
-	@Override
-	public void delete(Appointment appointment) throws SQLException, Exception {
-		Connection connection = DriverManager.getConnection(BBDDUtils.URL_LOCAL, BBDDUtils.USER_LOCAL,
-				BBDDUtils.PASS_LOCAL);
-		PreparedStatement preparedStatement = null;
-
-		Class.forName(BBDDUtils.DRIVER_LOCAL);
-		String sql = "delete from " + APPOINTMENT_TABLE + " where id = ?";
-		preparedStatement = connection.prepareStatement(sql);
-		preparedStatement.setInt(1, appointment.getId());
-
-		preparedStatement.executeUpdate();
-
-		if (preparedStatement != null)
-			preparedStatement.close();
-		if (connection != null)
-			connection.close();
-	}
-
-//	No sé hacer este
-	@Override
-	public void update(Appointment appointment, TimeSlot timeSlot) throws SQLException, Exception {
-		Connection connection = DriverManager.getConnection(BBDDUtils.URL_LOCAL, BBDDUtils.USER_LOCAL, BBDDUtils.PASS_LOCAL);
-		PreparedStatement preparedStatement = null;
-
-		Class.forName(BBDDUtils.DRIVER_LOCAL);
-
-//		String sql = "update "+APPOINTMENT_TABLE+" set idFranja = ? where id = ?";
-//		preparedStatement = connection.prepareStatement(sql);
-//		preparedStatement.setInt(1, 1);
-//		preparedStatement.setint(2, appointment.getId());
-
-		preparedStatement.executeUpdate();
-
-		if (preparedStatement != null)
-			preparedStatement.close();
-
-		if (connection != null)
-			connection.close();
 		
 	}
-	
 
+	@Override
+	public void insert(Appointment t) throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Appointment t) throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(int id) throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		
+	}
 }
