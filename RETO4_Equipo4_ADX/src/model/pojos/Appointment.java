fee438bb53be6a346e81292990b02d393db7f849
in1dw3 <in1dw3@HZ352016.elorrieta.int>
2023-04-25 16:44:00 +0200

    appointment

--- RETO4_Equipo4_ADX/src/model/pojos/Appointment.java
+++ RETO4_Equipo4_ADX/src/model/pojos/Appointment.java
@@ -1,7 +1,6 @@
 package model.pojos;
 
 import java.io.Serializable;
-import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Objects;
 
@@ -13,83 +12,49 @@ public class Appointment implements Serializable {
 	private static final long serialVersionUID = -406540892233438477L;
 
 	private int id;
-	private String type;
 
 	private ArrayList<Sanitarian> sanitarians;
 	private Patient patient;
 	private TimeSlot timeSlot;
 	private ArrayList<Ambulatory> ambulatory;
-
-	public Appointment() {
-
-	}
-
-	public Appointment(int id, String type, ArrayList<Sanitarian> sanitarians, Patient patient, TimeSlot timeSlot) {
-		super();
-		this.id = id;
-		this.type = type;
-		this.sanitarians = sanitarians;
-		this.patient = patient;
-		this.timeSlot = timeSlot;
-	}
-
+	
 	public int getId() {
 		return id;
 	}
-
 	public void setId(int id) {
 		this.id = id;
 	}
-
-	public String getType() {
-		return type;
-	}
-
-	public void setType(String type) {
-		this.type = type;
-	}
-
 	public ArrayList<Sanitarian> getSanitarians() {
 		return sanitarians;
 	}
-
 	public void setSanitarians(ArrayList<Sanitarian> sanitarians) {
 		this.sanitarians = sanitarians;
 	}
-
 	public Patient getPatient() {
 		return patient;
 	}
-
 	public void setPatient(Patient patient) {
 		this.patient = patient;
 	}
-
 	public TimeSlot getTimeSlot() {
 		return timeSlot;
 	}
-
 	public void setTimeSlot(TimeSlot timeSlot) {
 		this.timeSlot = timeSlot;
 	}
-
-	public static long getSerialversionuid() {
-		return serialVersionUID;
-	}
-	
 	public ArrayList<Ambulatory> getAmbulatory() {
 		return ambulatory;
 	}
-
 	public void setAmbulatory(ArrayList<Ambulatory> ambulatory) {
 		this.ambulatory = ambulatory;
 	}
-
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
 	@Override
 	public int hashCode() {
-		return Objects.hash(ambulatory, id, patient, sanitarians, timeSlot, type);
+		return Objects.hash(ambulatory, id, patient, sanitarians, timeSlot);
 	}
-
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -100,14 +65,13 @@ public class Appointment implements Serializable {
 			return false;
 		Appointment other = (Appointment) obj;
 		return Objects.equals(ambulatory, other.ambulatory) && id == other.id && Objects.equals(patient, other.patient)
-				&& Objects.equals(sanitarians, other.sanitarians) && Objects.equals(timeSlot, other.timeSlot)
-				&& Objects.equals(type, other.type);
+				&& Objects.equals(sanitarians, other.sanitarians) && Objects.equals(timeSlot, other.timeSlot);
 	}
-
 	@Override
 	public String toString() {
-		return "Appointment [id=" + id + ", type=" + type + ", sanitarians=" + sanitarians + ", patient=" + patient
-				+ ", timeSlot=" + timeSlot + ", ambulatory=" + ambulatory + "]";
+		return "Appointment [id=" + id + ", sanitarians=" + sanitarians + ", patient=" + patient + ", timeSlot="
+				+ timeSlot + ", ambulatory=" + ambulatory + "]";
 	}
+
 	
 }
