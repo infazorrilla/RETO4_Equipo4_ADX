8f8652dd8bf90ef3e8ce3c8d1d1f7ebfa6f97167
in1dw3 <in1dw3@HZ352014.elorrieta.int>
2023-04-28 19:02:08 +0300

    Creación de JUnits TimeSlotManegerTest y WorkingDayManagerTest (sin desarrollar, porque no tenemos datos en la base de datos)

---
+++ RETO4_Equipo4_ADX/src/jUnitTest/WorkingDayManagerTest.java
@@ -0,0 +1,21 @@
+package jUnitTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class WorkingDayManagerTest {
+	
+//	Todavía no tenemos datos en la base de datos
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
