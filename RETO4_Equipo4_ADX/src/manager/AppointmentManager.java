f353632b7765ddb2300b26b0d76abf6c65d8adac
in1dw3 <in1dw3@HZ352016.elorrieta.int>
2023-04-28 19:09:37 +0200

    AppointmentManager doit

--- RETO4_Equipo4_ADX/src/manager/AppointmentManager.java
+++ RETO4_Equipo4_ADX/src/manager/AppointmentManager.java
@@ -2,6 +2,7 @@ package manager;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -37,7 +38,6 @@ public class AppointmentManager extends AbstractManager<Appointment> {
 				if (null == ret)
 					ret = new Appointment();
 
-				
 				ret.setId(id);
 
 			}
@@ -76,55 +76,155 @@ public class AppointmentManager extends AbstractManager<Appointment> {
 		Statement statement = null;
 		ResultSet resultSet = null;
 
-		Class.forName(BBDDUtils.DRIVER_LOCAL);
-		connection = DriverManager.getConnection(BBDDUtils.URL_LOCAL, BBDDUtils.USER_LOCAL, BBDDUtils.PASS_LOCAL);
-		statement = connection.createStatement();
-		resultSet = statement.executeQuery(sql);
+		try {
+		
+			Class.forName(BBDDUtils.DRIVER_LOCAL);
+			connection = DriverManager.getConnection(BBDDUtils.URL_LOCAL, BBDDUtils.USER_LOCAL, BBDDUtils.PASS_LOCAL);
+			statement = connection.createStatement();
+			resultSet = statement.executeQuery(sql);
+		
+		while (resultSet.next()) {
+			if (null == ret)
+				ret = new ArrayList<Appointment>();
 
+				int id = resultSet.getInt("id");
+				
+				Appointment appointment= new Appointment();
+				appointment.setId(id);
+				
+			ret.add(appointment);
+		}
+
+		} catch (SQLException sqle) {
+		} catch (Exception e) {
+		} finally {
+			try {
+				if (resultSet != null)
+					resultSet.close();
+			} catch (Exception e) {
+			}
+			;
+			try {
+				if (statement != null)
+					statement.close();
+			} catch (Exception e) {
+			}
+			;
+			try {
+				if (connection != null)
+					connection.close();
+			} catch (Exception e) {
+			}
+			;
+		}
+		return ret;
 	}
 
-	resultSet.close();
 
-	if(statement!=null)statement.close();
-
-	if(connection!=null)connection.close();
-
-	return ret;
-
-	}
+	
 
 	@Override
 	public void insert(Appointment appointment) throws SQLException, Exception {
-		
-		Connection connection = DriverManager.getConnection(BBDDUtils.URL_LOCAL, BBDDUtils.USER_LOCAL,
-				BBDDUtils.PASS_LOCAL);
-		Statement statement = connection.createStatement();
+		Connection connection = null;
+		Statement statement = null;
+	try {	
+		 connection = DriverManager.getConnection(BBDDUtils.URL_LOCAL, BBDDUtils.USER_LOCAL,BBDDUtils.PASS_LOCAL);
+		 statement = connection.createStatement();
 		Class.forName(BBDDUtils.DRIVER_LOCAL);
 
-		String sql = "insert into " + APPOINTMENT_TABLE + " (id) values ('"
-				+ appointment.getPatient.getDniPatient()  + "', '" + appointment.getSanitarian.getDniSanitarian() + "', '" 
-				+ appointment.getAmbulatory.getId  + "', '" + appointment.getId() +  "')";
-				
+		String sql = "insert into " + APPOINTMENT_TABLE + " (id) values ('" + /**appointment.getPatient.getDniPatient()
+				+ "', '" + appointment.getSanitarian.getDniSanitarian() + "', '" + appointment.getAmbulatory.getId
+				+ "', '" +**/ appointment.getId() + "')";
 
 		statement.executeUpdate(sql);
 
-		if (statement != null)
-			statement.close();
-
-		if (connection != null)
-			connection.close();
-		
+	} catch (SQLException sqle) {
+	} catch (Exception e) {
+	} finally {
+		try {
+			if (statement != null)
+				statement.close();
+		} catch (Exception e) {
+		}
+		;
+		try {
+			if (connection != null)
+				connection.close();
+		} catch (Exception e) {
+		}
+		;
+	}
 	}
 
 	@Override
 	public void update(Appointment appointment) throws SQLException, Exception {
-		
-		
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+
+		try {
+			connection = DriverManager.getConnection(BBDDUtils.URL_LOCAL, BBDDUtils.USER_LOCAL, BBDDUtils.PASS_LOCAL);
+			preparedStatement = null;
+
+			Class.forName(BBDDUtils.DRIVER_LOCAL);
+			
+			int id= 8000;
+
+			 String sql = "update "+ APPOINTMENT_TABLE +" set id = ? where id = ?";
+			 preparedStatement = connection.prepareStatement(sql);
+			 preparedStatement.setInt(1, id);
+			 preparedStatement.setInt(2, appointment.getId());
+
+			preparedStatement.executeUpdate();
+
+		} catch (SQLException sqle) {
+		} catch (Exception e) {
+		} finally {
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+			} catch (Exception e) {
+			}
+			;
+			try {
+				if (connection != null)
+					connection.close();
+			} catch (Exception e) {
+			}
+			;
+		}
 	}
 
 	@Override
 	public void delete(int id) throws SQLException, Exception {
-		// TODO Auto-generated method stub
-		
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+
+		try {
+			connection = DriverManager.getConnection(BBDDUtils.URL_LOCAL, BBDDUtils.USER_LOCAL,
+					BBDDUtils.PASS_LOCAL);
+			preparedStatement = null;
+	
+			Class.forName(BBDDUtils.DRIVER_LOCAL);
+			String sql = "delete from " + APPOINTMENT_TABLE + " where id = " + id;
+			preparedStatement = connection.prepareStatement(sql);
+	
+			preparedStatement.executeUpdate();
+
+		} catch (SQLException sqle) {  
+		} catch(Exception e){ 
+		} finally {
+			try {
+				if (preparedStatement != null) 
+					preparedStatement.close(); 
+			} catch(Exception e){ 
+			};
+			try {
+				if (connection != null) 
+					connection.close(); 
+			} catch(Exception e){ 
+			};					
+		}
 	}
+
+
 }
