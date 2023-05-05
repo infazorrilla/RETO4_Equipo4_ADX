58ad097a5c0c77caf4018349d3cc8b1ae43c0f1f
dannyelfloyd <dannyelfloyd@gmail.com>
2023-05-05 12:11:26 +0200

    MANAGERS: DOCTOR AND NURSE + DELETE SANITARIAN

--- RETO4_Equipo4_ADX/src/manager/DoctorManager.java
+++ RETO4_Equipo4_ADX/src/manager/DoctorManager.java
@@ -1,9 +1,18 @@
 package manager;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
+import model.pojos.Ambulatory;
 import model.pojos.Doctor;
+import model.utils.BBDDUtils;
 
 public class DoctorManager extends AbstractManager<Doctor> {
 
@@ -21,33 +30,311 @@ public class DoctorManager extends AbstractManager<Doctor> {
 	 * student | An operation that affects several tables, for example
 	 */
 
+	public static final String SANITARIAN_TABLE = "sanitario";
+
 	@Override
 	public Doctor select(int id) throws SQLException, Exception {
-		// TODO Auto-generated method stub
-		return null;
+		Doctor ret = null;
+
+		String sql = "select * from " + SANITARIAN_TABLE + " where id=" + id + "' and tipo='Medicina'";
+
+		Connection connection = null;
+		Statement statement = null;
+		ResultSet resultSet = null;
+
+		try {
+			Class.forName(BBDDUtils.DRIVER_LOCAL);
+			connection = DriverManager.getConnection(BBDDUtils.URL_LOCAL, BBDDUtils.USER_LOCAL, BBDDUtils.PASS_LOCAL);
+			statement = connection.createStatement();
+			resultSet = statement.executeQuery(sql);
+
+			while (resultSet.next()) {
+				if (null == ret)
+					ret = new Doctor();
+
+				ret.setStaffNum(id);
+
+			}
+		} catch (SQLException sqle) {
+		} catch (Exception e) {
+		} finally {
+			try {
+				if (resultSet != null)
+					resultSet.close();
+			} catch (Exception e) {
+			}
+			;
+			try {
+				if (statement != null)
+					statement.close();
+			} catch (Exception e) {
+			}
+			;
+			try {
+				if (connection != null)
+					connection.close();
+			} catch (Exception e) {
+			}
+			;
+		}
+		return ret;
 	}
 
 	@Override
 	public List<Doctor> select() throws SQLException, Exception {
-		// TODO Auto-generated method stub
-		return null;
+		// Returns all rows of the ambulatory table
+		// If there is nothing, it returns NULL
+
+		ArrayList<Doctor> ret = null;
+
+		// SQL we want to launch
+		String sql = "select * from" + SANITARIAN_TABLE;
+
+		// The connection with BBDD
+		Connection connection = null;
+
+		// SQL statement against BBDD
+		// Result set will contain everything returned by the BBDD
+		Statement statement = null;
+		ResultSet resultSet = null;
+
+		try {
+			// The Driver we are going to use
+			Class.forName(BBDDUtils.DRIVER_LOCAL);
+
+			// Open the connection with BBDD
+			connection = DriverManager.getConnection(BBDDUtils.URL_LOCAL, BBDDUtils.USER_LOCAL, BBDDUtils.PASS_LOCAL);
+
+			// Let's launch the sentence. . .
+			statement = connection.createStatement();
+			resultSet = statement.executeQuery(sql);
+
+			// It is not possible to know how many things the resultSet has returned.
+			// You have to go 1 by 1 and save everything in its object
+			while (resultSet.next()) {
+
+				// If necessary, we initialize the list
+				if (null == ret)
+					ret = new ArrayList<Doctor>();
+
+				Doctor doctor = new Doctor();
+
+				// We take out the columns of the RS
+				String dni = resultSet.getString("dni");
+				int staffNum = resultSet.getInt("numPersonal");
+				float salary = resultSet.getFloat("salario");
+				int idAmbulatory = resultSet.getInt("idAmbulatorio");
+				String type = resultSet.getString("tipo");
+				String speciality = resultSet.getString("especialidad");
+				String mir = resultSet.getString("MIR");
+				String name = resultSet.getString("nombre");
+				String surname = resultSet.getString("apellido");
+				String gender = resultSet.getString("sexo");
+				Date birthDate = resultSet.getDate("fechaNac");
+				String password = resultSet.getString("contrasena");
+
+				// We put the data into Doctor
+				Ambulatory ambulatory = new Ambulatory();
+				ambulatory.setId(idAmbulatory);
+
+				doctor.setDni(dni);
+				doctor.setStaffNum(staffNum);
+				doctor.setSalary(salary);
+				doctor.setAmbulatory(ambulatory);
+				doctor.setType(type);
+				doctor.setSpeciality(speciality);
+				if (mir.equalsIgnoreCase("true")) {
+					doctor.setMir(true);
+				} else {
+					doctor.setMir(false);
+				}
+				doctor.setName(name);
+				doctor.setSurname(surname);
+				doctor.setGender(gender);
+				doctor.setBirthDate(birthDate);
+				doctor.setPassword(password);
+
+				// We save it in ret
+				ret.add(doctor);
+			}
+		} catch (SQLException sqle) {
+			System.out.println("Error with BBDD - " + sqle.getMessage());
+		} catch (Exception e) {
+			System.out.println("Generic error - " + e.getMessage());
+		} finally {
+			// We close in reverse of how we open them
+			try {
+				if (resultSet != null)
+					resultSet.close();
+			} catch (Exception e) {
+				// No need
+			}
+			;
+			try {
+				if (statement != null)
+					statement.close();
+			} catch (Exception e) {
+				// No need
+			}
+			;
+			try {
+				if (connection != null)
+					connection.close();
+			} catch (Exception e) {
+				// No need
+			}
+			;
+		}
+		return ret;
 	}
 
 	@Override
-	public void insert(Doctor t) throws SQLException, Exception {
-		// TODO Auto-generated method stub
+	public void insert(Doctor doctor) throws SQLException, Exception {
+		// Connection with the BD
+		Connection connection = null;
+
+		// SQL statement to BD
+		Statement statement = null;
+
+		try {
+			// The Driver that we are going to use
+			Class.forName(BBDDUtils.DRIVER_LOCAL);
+
+			// We open the connection with the BD
+			connection = DriverManager.getConnection(BBDDUtils.URL_LOCAL, BBDDUtils.USER_LOCAL, BBDDUtils.PASS_LOCAL);
+
+			// We are going to throw the statement
+			statement = connection.createStatement();
+
+			// SQL structure
+			String sql = "INSERT INTO " + SANITARIAN_TABLE
+					+ " (`dniSanitario`, `numPersonal`, `salario`, `idAmbulatorio`, `tipo`, `especialidad`, `MIR`, `categoria`, `EIR`) VALUES ('"
+					+ doctor.getDni() + "', '" + doctor.getStaffNum() + "', '" + doctor.getSalary() + "', 'NULL', '"
+					+ doctor.getType() + "', '" + doctor.getSpeciality() + "', '" + doctor.isMir() + "', NULL, NULL);";
+
+			// We execute
+			statement.executeUpdate(sql);
+
+		} catch (SQLException sqle) {
+			System.out.println("Error with the BD - " + sqle.getMessage());
+		} catch (Exception e) {
+			System.out.println("General Error - " + e.getMessage());
+		} finally {
+			// We close in reverse of how we open them
+			try {
+				if (statement != null)
+					statement.close();
+			} catch (Exception e) {
+				// Don't care
+			}
+			;
+			try {
+				if (connection != null)
+					connection.close();
+			} catch (Exception e) {
+				// Don't care
+			}
+			;
+		}
 
 	}
 
 	@Override
-	public void update(Doctor t) throws SQLException, Exception {
-		// TODO Auto-generated method stub
+	public void update(Doctor doctor) throws SQLException, Exception {
+		// Connection with the BD
+		Connection connection = null;
+
+		// SQL statement to BD
+		PreparedStatement preparedStatement = null;
+
+		try {
+			// The Driver that we are going to use
+			Class.forName(BBDDUtils.DRIVER_LOCAL);
+
+			// We open the connection with the BD
+			connection = DriverManager.getConnection(BBDDUtils.URL_LOCAL, BBDDUtils.USER_LOCAL, BBDDUtils.PASS_LOCAL);
+
+			String speciality = null;
+
+			// SQL structure.
+			// The ?s are filled in below
+			String sql = "update " + SANITARIAN_TABLE + " set especialidad = ? where dni = ?";
+
+			preparedStatement = connection.prepareStatement(sql);
+
+			preparedStatement.setString(1, speciality);
+			preparedStatement.setString(2, doctor.getDni());
+
+			// We execute
+			preparedStatement.executeUpdate();
+
+		} catch (SQLException sqle) {
+			System.out.println("Error with the BD - " + sqle.getMessage());
+		} catch (Exception e) {
+			System.out.println("Error generico - " + e.getMessage());
+		} finally {
+			// We close in reverse of how we open them
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+			} catch (Exception e) {
+				// Don't care
+			}
+			;
+			try {
+				if (connection != null)
+					connection.close();
+			} catch (Exception e) {
+				// Don't care
+			}
+			;
+		}
 
 	}
 
 	@Override
 	public void delete(int id) throws SQLException, Exception {
-		// TODO Auto-generated method stub
+		// Connection with the BD
+		Connection connection = null;
+
+		// SQL statement to BD
+		PreparedStatement preparedStatement = null;
+
+		try {
+			// The Driver that we are going to use
+			Class.forName(BBDDUtils.DRIVER_LOCAL);
+
+			// We open the connection with the BD
+			connection = DriverManager.getConnection(BBDDUtils.URL_LOCAL, BBDDUtils.USER_LOCAL, BBDDUtils.PASS_LOCAL);
+
+			// SQL structure
+			String sql = "delete from " + SANITARIAN_TABLE + " where dniSanitario = '" + id;
+			preparedStatement = connection.prepareStatement(sql);
+
+			// We execute
+			preparedStatement.executeUpdate();
+
+		} catch (SQLException sqle) {
+			System.out.println("Error with the BD - " + sqle.getMessage());
+		} catch (Exception e) {
+			System.out.println("General Error - " + e.getMessage());
+		} finally {
+			// We close in reverse of how we open them
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+			} catch (Exception e) {
+				// Don't care
+			}
+			;
+			try {
+				if (connection != null)
+					connection.close();
+			} catch (Exception e) {
+				// Don't care
+			}
+			;
+		}
 
 	}
 }
--- RETO4_Equipo4_ADX/src/manager/NurseManager.java
+++ RETO4_Equipo4_ADX/src/manager/NurseManager.java
@@ -1,9 +1,19 @@
 package manager;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
+import model.pojos.Ambulatory;
+import model.pojos.Doctor;
 import model.pojos.Nurse;
+import model.utils.BBDDUtils;
 
 public class NurseManager extends AbstractManager<Nurse> {
 
@@ -21,33 +31,312 @@ public class NurseManager extends AbstractManager<Nurse> {
 	 * student | An operation that affects several tables, for example
 	 */
 
+	public static final String SANITARIAN_TABLE = "sanitario";
+
 	@Override
 	public Nurse select(int id) throws SQLException, Exception {
-		// TODO Auto-generated method stub
-		return null;
+		Nurse ret = null;
+
+		String sql = "select * from " + SANITARIAN_TABLE + " where id=" + id + "' and tipo='Enfermeria'";
+
+		Connection connection = null;
+		Statement statement = null;
+		ResultSet resultSet = null;
+
+		try {
+			Class.forName(BBDDUtils.DRIVER_LOCAL);
+			connection = DriverManager.getConnection(BBDDUtils.URL_LOCAL, BBDDUtils.USER_LOCAL, BBDDUtils.PASS_LOCAL);
+			statement = connection.createStatement();
+			resultSet = statement.executeQuery(sql);
+
+			while (resultSet.next()) {
+				if (null == ret)
+					ret = new Nurse();
+
+				ret.setStaffNum(id);
+
+			}
+		} catch (SQLException sqle) {
+		} catch (Exception e) {
+		} finally {
+			try {
+				if (resultSet != null)
+					resultSet.close();
+			} catch (Exception e) {
+			}
+			;
+			try {
+				if (statement != null)
+					statement.close();
+			} catch (Exception e) {
+			}
+			;
+			try {
+				if (connection != null)
+					connection.close();
+			} catch (Exception e) {
+			}
+			;
+		}
+		return ret;
 	}
 
 	@Override
 	public List<Nurse> select() throws SQLException, Exception {
-		// TODO Auto-generated method stub
-		return null;
+		// Returns all rows of the ambulatory table
+		// If there is nothing, it returns NULL
+
+		ArrayList<Nurse> ret = null;
+
+		// SQL we want to launch
+		String sql = "select * from" + SANITARIAN_TABLE;
+
+		// The connection with BBDD
+		Connection connection = null;
+
+		// SQL statement against BBDD
+		// Result set will contain everything returned by the BBDD
+		Statement statement = null;
+		ResultSet resultSet = null;
+
+		try {
+			// The Driver we are going to use
+			Class.forName(BBDDUtils.DRIVER_LOCAL);
+
+			// Open the connection with BBDD
+			connection = DriverManager.getConnection(BBDDUtils.URL_LOCAL, BBDDUtils.USER_LOCAL, BBDDUtils.PASS_LOCAL);
+
+			// Let's launch the sentence. . .
+			statement = connection.createStatement();
+			resultSet = statement.executeQuery(sql);
+
+			// It is not possible to know how many things the resultSet has returned.
+			// You have to go 1 by 1 and save everything in its object
+			while (resultSet.next()) {
+
+				// If necessary, we initialize the list
+				if (null == ret)
+					ret = new ArrayList<Nurse>();
+
+				Nurse nurse = new Nurse();
+
+				// We take out the columns of the RS
+				String dni = resultSet.getString("dni");
+				int staffNum = resultSet.getInt("numPersonal");
+				float salary = resultSet.getFloat("salario");
+				int idAmbulatory = resultSet.getInt("idAmbulatorio");
+				String type = resultSet.getString("tipo");
+				String category = resultSet.getString("categoria");
+				String mir = resultSet.getString("MIR");
+				String name = resultSet.getString("nombre");
+				String surname = resultSet.getString("apellido");
+				String gender = resultSet.getString("sexo");
+				Date birthDate = resultSet.getDate("fechaNac");
+				String password = resultSet.getString("contrasena");
+
+				// We put the data into Doctor
+				Ambulatory ambulatory = new Ambulatory();
+				ambulatory.setId(idAmbulatory);
+
+				nurse.setDni(dni);
+				nurse.setStaffNum(staffNum);
+				nurse.setSalary(salary);
+				nurse.setAmbulatory(ambulatory);
+				nurse.setType(type);
+				nurse.setCategory(category);
+				if (mir.equalsIgnoreCase("true")) {
+					nurse.setEir(true);
+				} else {
+					nurse.setEir(false);
+				}
+				nurse.setName(name);
+				nurse.setSurname(surname);
+				nurse.setGender(gender);
+				nurse.setBirthDate(birthDate);
+				nurse.setPassword(password);
+
+				// We save it in ret
+				ret.add(nurse);
+			}
+		} catch (SQLException sqle) {
+			System.out.println("Error with BBDD - " + sqle.getMessage());
+		} catch (Exception e) {
+			System.out.println("Generic error - " + e.getMessage());
+		} finally {
+			// We close in reverse of how we open them
+			try {
+				if (resultSet != null)
+					resultSet.close();
+			} catch (Exception e) {
+				// No need
+			}
+			;
+			try {
+				if (statement != null)
+					statement.close();
+			} catch (Exception e) {
+				// No need
+			}
+			;
+			try {
+				if (connection != null)
+					connection.close();
+			} catch (Exception e) {
+				// No need
+			}
+			;
+		}
+		return ret;
+
 	}
 
 	@Override
-	public void insert(Nurse t) throws SQLException, Exception {
-		// TODO Auto-generated method stub
+	public void insert(Nurse nurse) throws SQLException, Exception {
+		// Connection with the BD
+		Connection connection = null;
+
+		// SQL statement to BD
+		Statement statement = null;
+
+		try {
+			// The Driver that we are going to use
+			Class.forName(BBDDUtils.DRIVER_LOCAL);
+
+			// We open the connection with the BD
+			connection = DriverManager.getConnection(BBDDUtils.URL_LOCAL, BBDDUtils.USER_LOCAL, BBDDUtils.PASS_LOCAL);
+
+			// We are going to throw the statement
+			statement = connection.createStatement();
+
+			// SQL structure
+			String sql = "INSERT INTO " + SANITARIAN_TABLE
+					+ " (`dniSanitario`, `numPersonal`, `salario`, `idAmbulatorio`, `tipo`, `especialidad`, `MIR`, `categoria`, `EIR`) VALUES ('"
+					+ nurse.getDni() + "', '" + nurse.getStaffNum() + "', '" + nurse.getSalary() + "', 'NULL', '"
+					+ nurse.getType() + "', 'NULL', 'NULL', " + nurse.getCategory() + ", " + nurse.isEir() + ");";
+
+			// We execute
+			statement.executeUpdate(sql);
+
+		} catch (SQLException sqle) {
+			System.out.println("Error with the BD - " + sqle.getMessage());
+		} catch (Exception e) {
+			System.out.println("General Error - " + e.getMessage());
+		} finally {
+			// We close in reverse of how we open them
+			try {
+				if (statement != null)
+					statement.close();
+			} catch (Exception e) {
+				// Don't care
+			}
+			;
+			try {
+				if (connection != null)
+					connection.close();
+			} catch (Exception e) {
+				// Don't care
+			}
+			;
+		}
 
 	}
 
 	@Override
-	public void update(Nurse t) throws SQLException, Exception {
-		// TODO Auto-generated method stub
+	public void update(Nurse nurse) throws SQLException, Exception {
+		// Connection with the BD
+		Connection connection = null;
+
+		// SQL statement to BD
+		PreparedStatement preparedStatement = null;
+
+		try {
+			// The Driver that we are going to use
+			Class.forName(BBDDUtils.DRIVER_LOCAL);
+
+			// We open the connection with the BD
+			connection = DriverManager.getConnection(BBDDUtils.URL_LOCAL, BBDDUtils.USER_LOCAL, BBDDUtils.PASS_LOCAL);
+
+			String category = null;
+
+			// SQL structure.
+			// The ?s are filled in below
+			String sql = "update " + SANITARIAN_TABLE + " set categoria = ? where dni = ?";
+
+			preparedStatement = connection.prepareStatement(sql);
+
+			preparedStatement.setString(1, category);
+			preparedStatement.setString(2, nurse.getDni());
+
+			// We execute
+			preparedStatement.executeUpdate();
+
+		} catch (SQLException sqle) {
+			System.out.println("Error with the BD - " + sqle.getMessage());
+		} catch (Exception e) {
+			System.out.println("Error generico - " + e.getMessage());
+		} finally {
+			// We close in reverse of how we open them
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+			} catch (Exception e) {
+				// Don't care
+			}
+			;
+			try {
+				if (connection != null)
+					connection.close();
+			} catch (Exception e) {
+				// Don't care
+			}
+			;
+		}
 
 	}
 
 	@Override
 	public void delete(int id) throws SQLException, Exception {
-		// TODO Auto-generated method stub
+		// Connection with the BD
+		Connection connection = null;
+
+		// SQL statement to BD
+		PreparedStatement preparedStatement = null;
+
+		try {
+			// The Driver that we are going to use
+			Class.forName(BBDDUtils.DRIVER_LOCAL);
+
+			// We open the connection with the BD
+			connection = DriverManager.getConnection(BBDDUtils.URL_LOCAL, BBDDUtils.USER_LOCAL, BBDDUtils.PASS_LOCAL);
+
+			// SQL structure
+			String sql = "delete from " + SANITARIAN_TABLE + " where dniSanitario = '" + id;
+			preparedStatement = connection.prepareStatement(sql);
+
+			// We execute
+			preparedStatement.executeUpdate();
+
+		} catch (SQLException sqle) {
+			System.out.println("Error with the BD - " + sqle.getMessage());
+		} catch (Exception e) {
+			System.out.println("General Error - " + e.getMessage());
+		} finally {
+			// We close in reverse of how we open them
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+			} catch (Exception e) {
+				// Don't care
+			}
+			;
+			try {
+				if (connection != null)
+					connection.close();
+			} catch (Exception e) {
+				// Don't care
+			}
+			;
+		}
 
 	}
 
