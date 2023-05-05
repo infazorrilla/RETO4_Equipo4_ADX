113102e8037b4d3f8c8ebb412e3da9293b9d5c2c
dannyelfloyd <dannyelfloyd@gmail.com>
2023-05-05 12:13:32 +0200

    CORRECTIONS IN MAIN

--- RETO4_Equipo4_ADX/src/Main.java
+++ RETO4_Equipo4_ADX/src/Main.java
@@ -19,8 +19,8 @@ public class Main {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					//ViewSabi window = new ViewSabi();
-					//ViewAmaia window = new ViewAmaia();
+					// ViewSabi window = new ViewSabi();
+					// ViewAmaia window = new ViewAmaia();
 					ViewDaniel window = new ViewDaniel();
 					window.frame.setVisible(true);
 				} catch (Exception e) {
@@ -28,30 +28,5 @@ public class Main {
 				}
 			}
 		});
-
-		// MANAGER VERIFICATION | AMBULATORY //
-	
-		/**
-		// SELECT
-		AmbulatoryManager ambulatoryManager = new AmbulatoryManager() ; // Instantiate
-		List<Ambulatory> ambulatory = AmbulatoryManager.select() ;
-		
-		for(Ambulatory ambulatoryForEach: ambulatory) {
-			System.out.println(ambulatoryForEach);
-		}
-		
-		// INSERT
-		AmbulatoryManager ambulatoryManager = new AmbulatoryManager() ; // Instantiate
-		Ambulatory insertAmbulatory = new Ambulatory (0, "Osakidetza", "Crueces", "945000000");
-		int registerAfected = ambulatoryManager.insert(insertAmbulatory);
-		List<Ambulatory> ambulatory = ambulatoryManager.select();
-		for(Ambulatory ambulatoryForEach: ambulatory) {
-			System.out.println(ambulatoryForEach);
-		}
-		
-		// UPDATE 
-		AmbulatoryManager ambulatoryManager = new AmbulatoryManager() ; // Instantiate
-		*/
-		
 	}
 }
