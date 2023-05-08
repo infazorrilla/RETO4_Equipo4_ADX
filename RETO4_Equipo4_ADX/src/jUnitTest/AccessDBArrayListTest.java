547f56821304d34d6303c1d012b37767fb9d7528
dannyelfloyd <dannyelfloyd@gmail.com>
2023-05-08 18:51:23 +0200

    CREATION OF NEW JUNIT FROM SPRINT 2

---
+++ RETO4_Equipo4_ADX/src/jUnitTest/AccessDBArrayListTest.java
@@ -0,0 +1,14 @@
+package jUnitTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AccessDBArrayListTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
